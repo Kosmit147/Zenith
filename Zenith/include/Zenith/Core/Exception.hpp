85216ff3c0706f70d99d675044dead62a9615557
Wojciech Opara <wwojtek660@gmail.com>
2024-11-17 22:52:04 +0100

    Remove Exception constructor which takes in a std::string_view
    std::string_view isn't guaranteed to be null-terminated which could cause problems

--- Zenith/include/Zenith/Core/Exception.hpp
+++ Zenith/include/Zenith/Core/Exception.hpp
@@ -3,7 +3,6 @@
 #include <stacktrace>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 
 #include "Zenith/Core/Typedefs.hpp"
 
@@ -13,7 +12,6 @@ class Exception : public std::runtime_error
 {
 public:
     explicit Exception(const char* message, usize skip = 0);
-    explicit Exception(std::string_view message, usize skip = 0);
     explicit Exception(const std::string& message, usize skip = 0);
 
     [[nodiscard]] auto stacktrace() const -> std::string;
