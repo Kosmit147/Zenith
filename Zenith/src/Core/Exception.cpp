a86dca491f80537632697b65eb1537ad26cefaa2
Wojciech Opara <wwojtek660@gmail.com>
2024-11-17 22:52:04 +0100

    Remove Exception constructor which takes in a std::string_view
    std::string_view isn't guaranteed to be null-terminated which could cause problems

--- Zenith/src/Core/Exception.cpp
+++ Zenith/src/Core/Exception.cpp
@@ -8,10 +8,6 @@ ZTH_NO_INLINE Exception::Exception(const char* message, usize skip)
     : std::runtime_error(message), _stacktrace(std::stacktrace::current(skip + 1))
 {}
 
-ZTH_NO_INLINE Exception::Exception(std::string_view message, usize skip)
-    : std::runtime_error(message.data()), _stacktrace(std::stacktrace::current(skip + 1))
-{}
-
 ZTH_NO_INLINE Exception::Exception(const std::string& message, usize skip)
     : std::runtime_error(message), _stacktrace(std::stacktrace::current(skip + 1))
 {}
