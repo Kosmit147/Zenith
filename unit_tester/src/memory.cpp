80c5ae254ce5926036421ec489f833f600957d3a
Wojciech Opara <wwojtek660@gmail.com>
2025-03-06 13:01:48 +0100

    Add memory module & functions for aligning pointers

---
+++ unit_tester/src/memory.cpp
@@ -0,0 +1,78 @@
+#include <array>
+#include <cstddef>
+
+#include <zenith/memory/memory.hpp>
+
+TEST_CASE("pointer alignment functions", "[Memory]")
+{
+    const std::array int_pointers = {
+        reinterpret_cast<int*>(0b0000), reinterpret_cast<int*>(0b0001), reinterpret_cast<int*>(0b0010),
+        reinterpret_cast<int*>(0b0011), reinterpret_cast<int*>(0b0100),
+    };
+
+    const std::array expected_aligned_int_pointers = {
+        reinterpret_cast<int*>(0b0000), reinterpret_cast<int*>(0b0100), reinterpret_cast<int*>(0b0100),
+        reinterpret_cast<int*>(0b0100), reinterpret_cast<int*>(0b0100),
+    };
+
+    const std::array expected_aligned_down_int_pointers = {
+        reinterpret_cast<int*>(0b0000), reinterpret_cast<int*>(0b0000), reinterpret_cast<int*>(0b0000),
+        reinterpret_cast<int*>(0b0000), reinterpret_cast<int*>(0b0100),
+    };
+
+    const std::array byte_pointers = {
+        reinterpret_cast<std::byte*>(0b0000),
+        reinterpret_cast<std::byte*>(0b0001),
+        reinterpret_cast<std::byte*>(0b0010),
+    };
+
+    const std::array expected_aligned_byte_pointers = {
+        reinterpret_cast<std::byte*>(0b0000),
+        reinterpret_cast<std::byte*>(0b0001),
+        reinterpret_cast<std::byte*>(0b0010),
+    };
+
+    const std::array expected_aligned_down_byte_pointers = {
+        reinterpret_cast<std::byte*>(0b0000),
+        reinterpret_cast<std::byte*>(0b0001),
+        reinterpret_cast<std::byte*>(0b0010),
+    };
+
+    SECTION("align")
+    {
+        for (std::size_t i = 0; i < int_pointers.size(); i++)
+        {
+            auto ptr = int_pointers[i];
+            REQUIRE(zth::aligned(ptr) == expected_aligned_int_pointers[i]);
+            zth::align(ptr);
+            REQUIRE(ptr == expected_aligned_int_pointers[i]);
+        }
+
+        for (std::size_t i = 0; i < byte_pointers.size(); i++)
+        {
+            auto ptr = byte_pointers[i];
+            REQUIRE(zth::aligned(ptr) == expected_aligned_byte_pointers[i]);
+            zth::align(ptr);
+            REQUIRE(ptr == expected_aligned_byte_pointers[i]);
+        }
+    }
+
+    SECTION("align_down")
+    {
+        for (std::size_t i = 0; i < int_pointers.size(); i++)
+        {
+            auto ptr = int_pointers[i];
+            REQUIRE(zth::aligned_down(ptr) == expected_aligned_down_int_pointers[i]);
+            zth::align_down(ptr);
+            REQUIRE(ptr == expected_aligned_down_int_pointers[i]);
+        }
+
+        for (std::size_t i = 0; i < byte_pointers.size(); i++)
+        {
+            auto ptr = byte_pointers[i];
+            REQUIRE(zth::aligned_down(ptr) == expected_aligned_down_byte_pointers[i]);
+            zth::align_down(ptr);
+            REQUIRE(ptr == expected_aligned_down_byte_pointers[i]);
+        }
+    }
+}
---
+++ zenith/include/zenith/memory/memory.inl
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "zenith/core/assert.hpp"
+#include "zenith/math/number.hpp"
+
+namespace zth {
+
+template<typename T> constexpr auto is_aligned(T* ptr, usize alignment) -> bool
+{
+    ZTH_ASSERT(is_power_of_2(alignment));
+    auto address = reinterpret_cast<uptr_t>(ptr);
+    auto mask = alignment - 1;
+    return !(address & mask);
+}
+
+template<typename T> constexpr auto aligned(T* ptr, usize alignment) -> T*
+{
+    ZTH_ASSERT(is_power_of_2(alignment));
+    auto address = reinterpret_cast<uptr_t>(ptr);
+    address += (alignment - 1);
+    return aligned_down(reinterpret_cast<T*>(address), alignment);
+}
+
+template<typename T> constexpr auto aligned_down(T* ptr, usize alignment) -> T*
+{
+    ZTH_ASSERT(is_power_of_2(alignment));
+    auto address = reinterpret_cast<uptr_t>(ptr);
+    auto mask = alignment - 1;
+    return reinterpret_cast<T*>(address & ~mask);
+}
+
+template<typename T> constexpr auto align(T*& ptr, usize alignment) -> usize
+{
+    ZTH_ASSERT(is_power_of_2(alignment));
+    auto address = reinterpret_cast<uptr_t>(ptr);
+    auto mask = alignment - 1;
+    auto offset = address & mask;
+
+    if (offset != 0)
+        offset = alignment - offset;
+
+    ptr = reinterpret_cast<T*>(address + offset);
+    return offset;
+}
+
+template<typename T> constexpr auto align_down(T*& ptr, usize alignment) -> usize
+{
+    ZTH_ASSERT(is_power_of_2(alignment));
+    auto address = reinterpret_cast<uptr_t>(ptr);
+    auto mask = alignment - 1;
+    auto offset = address & mask;
+    ptr = reinterpret_cast<T*>(address - offset);
+    return offset;
+}
+
+constexpr auto kilobytes(usize n) -> usize
+{
+    return n * 1024u;
+}
+
+constexpr auto megabytes(usize n) -> usize
+{
+    return kilobytes(n) * 1024u;
+}
+
+constexpr auto gigabytes(usize n) -> usize
+{
+    return megabytes(n) * 1024u;
+}
+
+namespace memory_size_literals {
+
+constexpr auto operator""_kb(usize n) -> usize
+{
+    return kilobytes(n);
+}
+
+constexpr auto operator""_mb(usize n) -> usize
+{
+    return megabytes(n);
+}
+
+constexpr auto operator""_gb(usize n) -> usize
+{
+    return gigabytes(n);
+}
+
+} // namespace memory_size_literals
+
+} // namespace zth
