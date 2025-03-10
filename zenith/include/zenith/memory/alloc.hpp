028ca8bc3171f5901032357265b3cd90c5e33fde
Wojciech Opara <wwojtek660@gmail.com>
2025-03-10 09:34:49 +0100

    Implement a deleter which uses our custom allocator

--- zenith/include/zenith/memory/alloc.hpp
+++ zenith/include/zenith/memory/alloc.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <concepts>
+#include <memory>
+#include <type_traits>
+
 #include "zenith/core/typedefs.hpp"
 
 namespace zth::memory {
@@ -23,16 +27,35 @@ template<typename T> auto deallocate(T*& ptr) noexcept -> void;
 // this function sets ptr to nullptr
 template<> auto deallocate(void*& ptr) noexcept -> void;
 
-template<typename T> struct Allocator
+// handles pointers to arrays, but not arrays of dynamic size (T[])
+// @todo: Handle arrays of dynamic size (T[]). We should take into account whether the type is trivially destructible,
+// because then we don't have to actually store the additional metadata nor call any destructors
+template<std::destructible T>
+    requires(!std::is_unbounded_array_v<T>)
+struct CustomDeleter
+{
+public:
+    constexpr CustomDeleter() noexcept = default;
+
+    template<typename U>
+    constexpr CustomDeleter(const CustomDeleter<U>&) noexcept
+        requires(std::convertible_to<U*, T*>)
+    {}
+
+    // std::destructible ensures that T's destructor doesn't throw
+    constexpr auto operator()(T* ptr) const noexcept -> void;
+};
+
+template<typename T> struct CustomAllocator
 {
     // A minimal allocator implementation.
 
     using value_type = T;
 
-    Allocator() noexcept = default;
-    template<typename U> Allocator(const Allocator<U>&) noexcept {}
-    template<typename U> auto operator==(const Allocator<U>&) const noexcept -> bool { return true; }
-    template<typename U> auto operator!=(const Allocator<U>&) const noexcept -> bool { return false; }
+    CustomAllocator() noexcept = default;
+    template<typename U> CustomAllocator(const CustomAllocator<U>&) noexcept {}
+    template<typename U> auto operator==(const CustomAllocator<U>&) const noexcept -> bool { return true; }
+    template<typename U> auto operator!=(const CustomAllocator<U>&) const noexcept -> bool { return false; }
 
     auto allocate(std::size_t count) const noexcept -> T*;
     auto deallocate(T* ptr, std::size_t count) const noexcept -> void;
@@ -53,12 +76,21 @@ template<typename T> auto deallocate(T*& ptr) noexcept -> void
     deallocate(reinterpret_cast<void*&>(ptr));
 }
 
-template<typename T> auto Allocator<T>::allocate(std::size_t count) const noexcept -> T*
+template<std::destructible T>
+    requires(!std::is_unbounded_array_v<T>)
+constexpr auto CustomDeleter<T>::operator()(T* ptr) const noexcept -> void
+{
+    std::destroy_at(ptr);
+    deallocate(ptr);
+}
+
+template<typename T> auto CustomAllocator<T>::allocate(std::size_t count) const noexcept -> T*
 {
     return static_cast<T*>(memory::allocate(count * sizeof(T)));
 }
 
-template<typename T> auto Allocator<T>::deallocate(T* ptr, [[maybe_unused]] std::size_t count) const noexcept -> void
+template<typename T>
+auto CustomAllocator<T>::deallocate(T* ptr, [[maybe_unused]] std::size_t count) const noexcept -> void
 {
     memory::deallocate(ptr);
 }
