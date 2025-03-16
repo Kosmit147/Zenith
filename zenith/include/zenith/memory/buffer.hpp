ce01855047dc765001a6b27996b7560a42a501cd
Wojciech Opara <wwojtek660@gmail.com>
2025-03-16 21:45:35 +0100

    Add noexcept specifiers

--- zenith/include/zenith/memory/buffer.hpp
+++ zenith/include/zenith/memory/buffer.hpp
@@ -35,33 +35,33 @@ public:
 
 public:
     explicit StaticBuffer() = default;
-    explicit StaticBuffer(const void* data, size_type data_size_bytes);
-    explicit StaticBuffer(std::ranges::contiguous_range auto&& data);
+    explicit StaticBuffer(const void* data, size_type data_size_bytes) noexcept;
+    explicit StaticBuffer(std::ranges::contiguous_range auto&& data) noexcept;
 
-    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) -> StaticBuffer;
-    [[nodiscard]] static auto with_data(auto&& data) -> StaticBuffer;
-    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) -> StaticBuffer;
+    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) noexcept -> StaticBuffer;
+    [[nodiscard]] static auto with_data(auto&& data) noexcept -> StaticBuffer;
+    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) noexcept -> StaticBuffer;
 
     ZTH_DEFAULT_COPY_DEFAULT_MOVE(StaticBuffer)
 
     ~StaticBuffer() = default;
 
     // --- ContiguousRange implementation
-    [[nodiscard]] auto data(this auto&& self) -> auto*;
-    [[nodiscard]] auto begin(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto end(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto cbegin() const -> const_iterator;
-    [[nodiscard]] auto cend() const -> const_iterator;
-    [[nodiscard]] static auto size() -> size_type { return Size; }
-    [[nodiscard]] static auto capacity() -> size_type { return Size; }
+    [[nodiscard]] auto data(this auto&& self) noexcept -> auto*;
+    [[nodiscard]] auto begin(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto end(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto cbegin() const noexcept -> const_iterator;
+    [[nodiscard]] auto cend() const noexcept -> const_iterator;
+    [[nodiscard]] static auto size() noexcept -> size_type { return Size; }
+    [[nodiscard]] static auto capacity() noexcept -> size_type { return Size; }
 
     // --- StaticBuffer implementation
     // Returns the number of bytes written.
-    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) -> size_type;
+    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(auto&& data, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) noexcept -> size_type;
 };
 
 class Buffer : public ContiguousRangeInterface
@@ -78,17 +78,17 @@ public:
     using const_iterator = const_pointer;
 
     explicit Buffer() = default;
-    explicit Buffer(size_type size_bytes);
-    explicit Buffer(const void* data, size_type data_size_bytes);
-    explicit Buffer(std::ranges::contiguous_range auto&& data);
+    explicit Buffer(size_type size_bytes) noexcept;
+    explicit Buffer(const void* data, size_type data_size_bytes) noexcept;
+    explicit Buffer(std::ranges::contiguous_range auto&& data) noexcept;
 
-    [[nodiscard]] static auto with_size(size_type size_bytes) -> Buffer;
-    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) -> Buffer;
-    [[nodiscard]] static auto with_data(auto&& data) -> Buffer;
-    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) -> Buffer;
+    [[nodiscard]] static auto with_size(size_type size_bytes) noexcept -> Buffer;
+    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) noexcept -> Buffer;
+    [[nodiscard]] static auto with_data(auto&& data) noexcept -> Buffer;
+    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) noexcept -> Buffer;
 
-    Buffer(const Buffer& other);
-    auto operator=(const Buffer& other) -> Buffer&;
+    Buffer(const Buffer& other) noexcept;
+    auto operator=(const Buffer& other) noexcept -> Buffer&;
 
     Buffer(Buffer&& other) noexcept;
     auto operator=(Buffer&& other) noexcept -> Buffer&;
@@ -96,24 +96,24 @@ public:
     ~Buffer();
 
     // --- ContiguousRange implementation
-    [[nodiscard]] auto data(this auto&& self) -> auto*;
-    [[nodiscard]] auto begin(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto end(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto cbegin() const -> const_iterator;
-    [[nodiscard]] auto cend() const -> const_iterator;
-    [[nodiscard]] auto size() const -> size_type { return _size_bytes; }
-    [[nodiscard]] auto capacity() const -> size_type { return _size_bytes; }
+    [[nodiscard]] auto data(this auto&& self) noexcept -> auto*;
+    [[nodiscard]] auto begin(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto end(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto cbegin() const noexcept -> const_iterator;
+    [[nodiscard]] auto cend() const noexcept -> const_iterator;
+    [[nodiscard]] auto size() const noexcept -> size_type { return _size_bytes; }
+    [[nodiscard]] auto capacity() const noexcept -> size_type { return _size_bytes; }
 
     // --- Buffer implementation
-    auto resize(size_type size_bytes) -> void;
-    auto free() -> void;
+    auto resize(size_type size_bytes) noexcept -> void;
+    auto free() noexcept -> void;
 
     // Returns the number of bytes written.
-    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) -> size_type;
+    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(auto&& data, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) noexcept -> size_type;
 
 private:
     size_type _size_bytes = 0;
@@ -121,8 +121,8 @@ private:
     // We're not using unique_ptr because a unique_ptr makes it more messy to reallocate the data.
 
 private:
-    auto allocate(size_type size_bytes) -> void;
-    auto reallocate(size_type size_bytes) -> void;
+    auto allocate(size_type size_bytes) noexcept -> void;
+    auto reallocate(size_type size_bytes) noexcept -> void;
 };
 
 class DynamicBuffer : public ContiguousRangeInterface
@@ -139,17 +139,17 @@ public:
     using const_iterator = const_pointer;
 
     explicit DynamicBuffer() = default;
-    explicit DynamicBuffer(size_type size_bytes);
-    explicit DynamicBuffer(const void* data, size_type data_size_bytes);
-    explicit DynamicBuffer(std::ranges::contiguous_range auto&& data);
+    explicit DynamicBuffer(size_type size_bytes) noexcept;
+    explicit DynamicBuffer(const void* data, size_type data_size_bytes) noexcept;
+    explicit DynamicBuffer(std::ranges::contiguous_range auto&& data) noexcept;
 
-    [[nodiscard]] static auto with_size(size_type size_bytes) -> DynamicBuffer;
-    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) -> DynamicBuffer;
-    [[nodiscard]] static auto with_data(auto&& data) -> DynamicBuffer;
-    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) -> DynamicBuffer;
+    [[nodiscard]] static auto with_size(size_type size_bytes) noexcept -> DynamicBuffer;
+    [[nodiscard]] static auto with_data(const void* data, size_type data_size_bytes) noexcept -> DynamicBuffer;
+    [[nodiscard]] static auto with_data(auto&& data) noexcept -> DynamicBuffer;
+    [[nodiscard]] static auto with_data(std::ranges::contiguous_range auto&& data) noexcept -> DynamicBuffer;
 
-    DynamicBuffer(const DynamicBuffer& other);
-    auto operator=(const DynamicBuffer& other) -> DynamicBuffer&;
+    DynamicBuffer(const DynamicBuffer& other) noexcept;
+    auto operator=(const DynamicBuffer& other) noexcept -> DynamicBuffer&;
 
     DynamicBuffer(DynamicBuffer&& other) noexcept;
     auto operator=(DynamicBuffer&& other) noexcept -> DynamicBuffer&;
@@ -157,28 +157,28 @@ public:
     ~DynamicBuffer();
 
     // --- ContiguousRange implementation
-    [[nodiscard]] auto data(this auto&& self) -> auto*;
-    [[nodiscard]] auto begin(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto end(this auto&& self) -> decltype(auto);
-    [[nodiscard]] auto cbegin() const -> const_iterator;
-    [[nodiscard]] auto cend() const -> const_iterator;
-    [[nodiscard]] auto size() const -> size_type { return _size_bytes; }
-    [[nodiscard]] auto capacity() const -> size_type { return _capacity_bytes; }
+    [[nodiscard]] auto data(this auto&& self) noexcept -> auto*;
+    [[nodiscard]] auto begin(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto end(this auto&& self) noexcept -> decltype(auto);
+    [[nodiscard]] auto cbegin() const noexcept -> const_iterator;
+    [[nodiscard]] auto cend() const noexcept -> const_iterator;
+    [[nodiscard]] auto size() const noexcept -> size_type { return _size_bytes; }
+    [[nodiscard]] auto capacity() const noexcept -> size_type { return _capacity_bytes; }
 
     // --- DynamicBuffer implementation
-    auto resize(size_type size_bytes) -> void;
-    auto reserve(size_type min_capacity_bytes) -> void;
-    auto shrink_to_fit() -> void;
-    auto free() -> void;
+    auto resize(size_type size_bytes) noexcept -> void;
+    auto reserve(size_type min_capacity_bytes) noexcept -> void;
+    auto shrink_to_fit() noexcept -> void;
+    auto free() noexcept -> void;
 
     // Returns the number of bytes written.
-    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) -> size_type;
+    auto buffer_data(const void* data, size_type data_size_bytes, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(auto&& data, size_type offset = 0) noexcept -> size_type;
     // Returns the number of bytes written.
-    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) -> size_type;
+    auto buffer_data(std::ranges::contiguous_range auto&& data, size_type offset = 0) noexcept -> size_type;
 
-    auto clear() -> void;
+    auto clear() noexcept -> void;
 
 private:
     size_type _size_bytes = 0;
@@ -187,13 +187,13 @@ private:
     // We're not using unique_ptr because a unique_ptr makes it more messy to reallocate the data.
 
 private:
-    auto resize_to_at_least(size_type min_size_bytes) -> void;
+    auto resize_to_at_least(size_type min_size_bytes) noexcept -> void;
 
-    auto allocate(size_type capacity_bytes) -> void;
-    auto reallocate_exactly(size_type new_capacity_bytes) -> void;
-    auto reallocate_at_least(size_type min_capacity_bytes) -> void;
+    auto allocate(size_type capacity_bytes) noexcept -> void;
+    auto reallocate_exactly(size_type new_capacity_bytes) noexcept -> void;
+    auto reallocate_at_least(size_type min_capacity_bytes) noexcept -> void;
 
-    [[nodiscard]] static auto calculate_growth(size_type old_size_bytes) -> size_type;
+    [[nodiscard]] static auto calculate_growth(size_type old_size_bytes) noexcept -> size_type;
 };
 
 // @todo: Implement SmallBuffer.
--- zenith/src/memory/buffer.cpp
+++ zenith/src/memory/buffer.cpp
@@ -5,32 +5,32 @@
 
 namespace zth::memory {
 
-Buffer::Buffer(size_type size_bytes)
+Buffer::Buffer(size_type size_bytes) noexcept
 {
     allocate(size_bytes);
 }
 
-Buffer::Buffer(const void* data, size_type data_size_bytes) : Buffer(data_size_bytes)
+Buffer::Buffer(const void* data, size_type data_size_bytes) noexcept : Buffer(data_size_bytes)
 {
     buffer_data(data, data_size_bytes);
 }
 
-auto Buffer::with_size(size_type size_bytes) -> Buffer
+auto Buffer::with_size(size_type size_bytes) noexcept -> Buffer
 {
     return Buffer{ size_bytes };
 }
 
-auto Buffer::with_data(const void* data, size_type data_size_bytes) -> Buffer
+auto Buffer::with_data(const void* data, size_type data_size_bytes) noexcept -> Buffer
 {
     return Buffer{ data, data_size_bytes };
 }
 
-Buffer::Buffer(const Buffer& other) : Buffer(other._size_bytes)
+Buffer::Buffer(const Buffer& other) noexcept : Buffer(other._size_bytes)
 {
     std::memcpy(_data, other._data, _size_bytes);
 }
 
-auto Buffer::operator=(const Buffer& other) -> Buffer&
+auto Buffer::operator=(const Buffer& other) noexcept -> Buffer&
 {
     if (this == &other)
         return *this;
@@ -64,17 +64,17 @@ Buffer::~Buffer()
     free();
 }
 
-auto Buffer::cbegin() const -> const_iterator
+auto Buffer::cbegin() const noexcept -> const_iterator
 {
     return begin();
 }
 
-auto Buffer::cend() const -> const_iterator
+auto Buffer::cend() const noexcept -> const_iterator
 {
     return end();
 }
 
-auto Buffer::resize(size_type size_bytes) -> void
+auto Buffer::resize(size_type size_bytes) noexcept -> void
 {
     if (_size_bytes == size_bytes)
         return;
@@ -82,57 +82,57 @@ auto Buffer::resize(size_type size_bytes) -> void
     reallocate(size_bytes);
 }
 
-auto Buffer::free() -> void
+auto Buffer::free() noexcept -> void
 {
     _size_bytes = 0;
     memory::deallocate(_data);
 }
 
-auto Buffer::buffer_data(const void* data, size_type data_size_bytes, size_type offset) -> size_type
+auto Buffer::buffer_data(const void* data, size_type data_size_bytes, size_type offset) noexcept -> size_type
 {
     ZTH_ASSERT(offset + data_size_bytes <= _size_bytes);
     std::memcpy(_data + offset, data, data_size_bytes);
     return data_size_bytes;
 }
 
-auto Buffer::allocate(size_type size_bytes) -> void
+auto Buffer::allocate(size_type size_bytes) noexcept -> void
 {
     _data = static_cast<byte*>(memory::allocate(size_bytes));
     _size_bytes = size_bytes;
 }
 
-auto Buffer::reallocate(size_type size_bytes) -> void
+auto Buffer::reallocate(size_type size_bytes) noexcept -> void
 {
     memory::reallocate(_data, size_bytes, _size_bytes);
     _size_bytes = size_bytes;
 }
 
-DynamicBuffer::DynamicBuffer(size_type size_bytes)
+DynamicBuffer::DynamicBuffer(size_type size_bytes) noexcept
 {
     resize(size_bytes);
 }
 
-DynamicBuffer::DynamicBuffer(const void* data, size_type data_size_bytes) : DynamicBuffer(data_size_bytes)
+DynamicBuffer::DynamicBuffer(const void* data, size_type data_size_bytes) noexcept : DynamicBuffer(data_size_bytes)
 {
     buffer_data(data, data_size_bytes);
 }
 
-auto DynamicBuffer::with_size(size_type size_bytes) -> DynamicBuffer
+auto DynamicBuffer::with_size(size_type size_bytes) noexcept -> DynamicBuffer
 {
     return DynamicBuffer{ size_bytes };
 }
 
-auto DynamicBuffer::with_data(const void* data, size_type data_size_bytes) -> DynamicBuffer
+auto DynamicBuffer::with_data(const void* data, size_type data_size_bytes) noexcept -> DynamicBuffer
 {
     return DynamicBuffer{ data, data_size_bytes };
 }
 
-DynamicBuffer::DynamicBuffer(const DynamicBuffer& other) : DynamicBuffer(other._size_bytes)
+DynamicBuffer::DynamicBuffer(const DynamicBuffer& other) noexcept : DynamicBuffer(other._size_bytes)
 {
     std::memcpy(_data, other._data, _size_bytes);
 }
 
-auto DynamicBuffer::operator=(const DynamicBuffer& other) -> DynamicBuffer&
+auto DynamicBuffer::operator=(const DynamicBuffer& other) noexcept -> DynamicBuffer&
 {
     if (this == &other)
         return *this;
@@ -170,28 +170,28 @@ DynamicBuffer::~DynamicBuffer()
     free();
 }
 
-auto DynamicBuffer::cbegin() const -> const_iterator
+auto DynamicBuffer::cbegin() const noexcept -> const_iterator
 {
     return begin();
 }
 
-auto DynamicBuffer::cend() const -> const_iterator
+auto DynamicBuffer::cend() const noexcept -> const_iterator
 {
     return end();
 }
 
-auto DynamicBuffer::resize(size_type size_bytes) -> void
+auto DynamicBuffer::resize(size_type size_bytes) noexcept -> void
 {
     reserve(size_bytes);
     _size_bytes = size_bytes;
 }
 
-auto DynamicBuffer::reserve(size_type min_capacity_bytes) -> void
+auto DynamicBuffer::reserve(size_type min_capacity_bytes) noexcept -> void
 {
     reallocate_at_least(min_capacity_bytes);
 }
 
-auto DynamicBuffer::shrink_to_fit() -> void
+auto DynamicBuffer::shrink_to_fit() noexcept -> void
 {
     ZTH_ASSERT(_size_bytes <= _capacity_bytes);
 
@@ -201,44 +201,44 @@ auto DynamicBuffer::shrink_to_fit() -> void
     reallocate_exactly(_size_bytes);
 }
 
-auto DynamicBuffer::free() -> void
+auto DynamicBuffer::free() noexcept -> void
 {
     _size_bytes = 0;
     _capacity_bytes = 0;
     memory::deallocate(_data);
 }
 
-auto DynamicBuffer::buffer_data(const void* data, size_type data_size_bytes, size_type offset) -> size_type
+auto DynamicBuffer::buffer_data(const void* data, size_type data_size_bytes, size_type offset) noexcept -> size_type
 {
     resize_to_at_least(offset + data_size_bytes);
     std::memcpy(_data + offset, data, data_size_bytes);
     return data_size_bytes;
 }
 
-auto DynamicBuffer::clear() -> void
+auto DynamicBuffer::clear() noexcept -> void
 {
     _size_bytes = 0;
 }
 
-auto DynamicBuffer::resize_to_at_least(size_type min_size_bytes) -> void
+auto DynamicBuffer::resize_to_at_least(size_type min_size_bytes) noexcept -> void
 {
     reserve(min_size_bytes);
     _size_bytes = std::max(_size_bytes, min_size_bytes);
 }
 
-auto DynamicBuffer::allocate(size_type capacity_bytes) -> void
+auto DynamicBuffer::allocate(size_type capacity_bytes) noexcept -> void
 {
     _data = static_cast<byte*>(memory::allocate(capacity_bytes));
     _capacity_bytes = capacity_bytes;
 }
 
-auto DynamicBuffer::reallocate_exactly(size_type new_capacity_bytes) -> void
+auto DynamicBuffer::reallocate_exactly(size_type new_capacity_bytes) noexcept -> void
 {
     memory::reallocate(_data, new_capacity_bytes, _size_bytes);
     _capacity_bytes = new_capacity_bytes;
 }
 
-auto DynamicBuffer::reallocate_at_least(size_type min_capacity_bytes) -> void
+auto DynamicBuffer::reallocate_at_least(size_type min_capacity_bytes) noexcept -> void
 {
     if (_capacity_bytes >= min_capacity_bytes)
         return;
@@ -247,7 +247,7 @@ auto DynamicBuffer::reallocate_at_least(size_type min_capacity_bytes) -> void
     reallocate_exactly(new_capacity);
 }
 
-auto DynamicBuffer::calculate_growth(size_type old_size_bytes) -> size_type
+auto DynamicBuffer::calculate_growth(size_type old_size_bytes) noexcept -> size_type
 {
     return old_size_bytes * 2;
 }
