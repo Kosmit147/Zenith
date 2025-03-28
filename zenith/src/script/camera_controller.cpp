5f7ad516a7106723105a1bfdfda54febe9c2d4df
Wojciech Opara <wwojtek660@gmail.com>
2025-03-28 10:46:42 +0100

    Enable moving around when cursor is enabled

--- zenith/src/script/camera_controller.cpp
+++ zenith/src/script/camera_controller.cpp
@@ -41,48 +41,53 @@ auto FlyCamera::debug_edit() -> void
 
 auto FlyCamera::on_update(EntityHandle actor) -> void
 {
-    if (Window::cursor_enabled())
-        return;
-
     auto& transform = actor.get<TransformComponent>();
 
-    auto delta_time = Time::delta_time<float>();
-    auto speed = movement_speed * delta_time;
-
-    if (sprinting_enabled)
     {
-        if (Input::is_key_pressed(sprint_key))
-            speed *= sprinting_speed_multiplier;
+        // Move around.
+
+        auto speed = movement_speed * Time::delta_time<float>();
+
+        if (sprinting_enabled)
+        {
+            if (Input::is_key_pressed(sprint_key))
+                speed *= sprinting_speed_multiplier;
+        }
+
+        auto forward = transform.forward() * speed;
+        auto backward = -forward;
+        auto right = transform.right() * speed;
+        auto left = -right;
+
+        if (Input::is_key_pressed(move_forward_key))
+            transform.translate(forward);
+
+        if (Input::is_key_pressed(move_backward_key))
+            transform.translate(backward);
+
+        if (Input::is_key_pressed(move_right_key))
+            transform.translate(right);
+
+        if (Input::is_key_pressed(move_left_key))
+            transform.translate(left);
     }
 
-    auto forward = transform.forward();
-    auto backward = -forward;
-    auto right = transform.right();
-    auto left = -right;
+    if (!Window::cursor_enabled())
+    {
+        // Look around.
 
-    if (Input::is_key_pressed(move_forward_key))
-        transform.translate(forward * speed);
+        auto mouse_delta = Input::mouse_pos_delta() / static_cast<float>(Window::size().y) * mouse_sensitivity;
+        auto angles = transform.euler_angles();
 
-    if (Input::is_key_pressed(move_backward_key))
-        transform.translate(backward * speed);
+        angles.pitch -= mouse_delta.y;
+        angles.yaw -= mouse_delta.x;
+        angles.roll = 0.0f;
 
-    if (Input::is_key_pressed(move_right_key))
-        transform.translate(right * speed);
+        if (clamp_pitch)
+            angles.pitch = std::clamp(angles.pitch, min_pitch, max_pitch);
 
-    if (Input::is_key_pressed(move_left_key))
-        transform.translate(left * speed);
-
-    auto mouse_delta = Input::mouse_pos_delta() / static_cast<float>(Window::size().y) * mouse_sensitivity;
-    auto angles = transform.euler_angles();
-
-    angles.pitch -= mouse_delta.y;
-    angles.yaw -= mouse_delta.x;
-    angles.roll = 0.0f;
-
-    if (clamp_pitch)
-        angles.pitch = std::clamp(angles.pitch, min_pitch, max_pitch);
-
-    transform.set_rotation(angles);
+        transform.set_rotation(angles);
+    }
 }
 
 } // namespace zth::scripts
