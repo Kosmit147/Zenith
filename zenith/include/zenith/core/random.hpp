35feb005223d219e8845d775b598b79d660d9df8
Wojciech Opara <wwojtek660@gmail.com>
2025-04-01 13:32:43 +0200

    Implement more random functions

--- zenith/include/zenith/core/random.hpp
+++ zenith/include/zenith/core/random.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <glm/gtc/noise.hpp>
+#include <glm/gtc/random.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
 #include <concepts>
 #include <numbers>
 #include <random>
 
 #include "zenith/core/assert.hpp"
 #include "zenith/core/typedefs.hpp"
+#include "zenith/math/quaternion.hpp"
 #include "zenith/stl/string.hpp"
 #include "zenith/util/result.hpp"
 
@@ -22,7 +28,7 @@ public:
     static auto shut_down() -> void;
 
     // Generates a random integer in range [min; max] (inclusive).
-    template<std::integral T = int> [[nodiscard]] static auto random_int(T min, T max) -> T
+    template<std::integral T = int> [[nodiscard]] static auto get_int(T min, T max) -> T
     {
         ZTH_ASSERT(min <= max);
         std::uniform_int_distribution dist(min, max);
@@ -30,7 +36,7 @@ public:
     }
 
     // Generates a random floating-point number in range [min; max).
-    template<std::floating_point T = float> [[nodiscard]] static auto random_float(T min = 0.0, T max = 1.0) -> T
+    template<std::floating_point T = float> [[nodiscard]] static auto get_float(T min = 0.0, T max = 1.0) -> T
     {
         ZTH_ASSERT(min <= max);
         std::uniform_real_distribution dist(min, max);
@@ -38,10 +44,58 @@ public:
     }
 
     // Generates a random floating-point number in range [0 rad; 6.28319 rad).
-    template<std::floating_point T = float> [[nodiscard]] static auto random_angle() -> T
+    template<std::floating_point T = float> [[nodiscard]] static auto angle() -> T
     {
         constexpr auto full_angle = std::numbers::pi_v<T> * 2;
-        return random_float<T>(0, full_angle);
+        return get_float<T>(0.0, full_angle);
+    }
+
+    template<std::floating_point T = float> [[nodiscard]] static auto point_on_sphere(T radius = 1.0) -> glm::vec<3, T>
+    {
+        return glm::sphericalRand(radius);
+    }
+
+    template<std::floating_point T = float>
+    [[nodiscard]] static auto point_inside_sphere(T radius = 1.0) -> glm::vec<3, T>
+    {
+        return glm::ballRand(radius);
+    }
+
+    template<std::floating_point T = float> [[nodiscard]] static auto point_on_circle(T radius = 1.0) -> glm::vec<2, T>
+    {
+        return glm::circularRand(radius);
+    }
+
+    template<std::floating_point T = float>
+    [[nodiscard]] static auto point_inside_circle(T radius = 1.0) -> glm::vec<2, T>
+    {
+        return glm::diskRand(radius);
+    }
+
+    [[nodiscard]] static auto rotation() -> math::AngleAxis
+    {
+        return math::AngleAxis{
+            .angle = angle(),
+            .axis = point_on_sphere(),
+        };
+    }
+
+    template<glm::length_t Size, std::floating_point T = float>
+        requires(Size > 1)
+    [[nodiscard]] static auto perlin_noise(glm::vec<Size, T> position) -> T
+    {
+        return glm::perlin(position);
+    }
+
+    template<std::floating_point T = float> [[nodiscard]] static auto normal(T mean = 0.0, T deviation = 1.0) -> T
+    {
+        return glm::gaussRand(mean, deviation);
+    }
+
+    template<std::floating_point T = float> [[nodiscard]] static auto linear(T min = 0.0, T max = 1.0) -> T
+    {
+        ZTH_ASSERT(min <= max);
+        return glm::linearRand(min, max);
     }
 
 private:
@@ -49,17 +103,40 @@ private:
     static std::mt19937_64 _generator;
 };
 
-extern template auto Random::random_int<u16>(u16, u16) -> u16;
-extern template auto Random::random_int<i16>(i16, i16) -> i16;
-extern template auto Random::random_int<u32>(u32, u32) -> u32;
-extern template auto Random::random_int<i32>(i32, i32) -> i32;
-extern template auto Random::random_int<u64>(u64, u64) -> u64;
-extern template auto Random::random_int<i64>(i64, i64) -> i64;
+extern template auto Random::get_int<u16>(u16, u16) -> u16;
+extern template auto Random::get_int<i16>(i16, i16) -> i16;
+extern template auto Random::get_int<u32>(u32, u32) -> u32;
+extern template auto Random::get_int<i32>(i32, i32) -> i32;
+extern template auto Random::get_int<u64>(u64, u64) -> u64;
+extern template auto Random::get_int<i64>(i64, i64) -> i64;
 
-extern template auto Random::random_float<float>(float, float) -> float;
-extern template auto Random::random_float<double>(double, double) -> double;
+extern template auto Random::get_float<float>(float, float) -> float;
+extern template auto Random::get_float<double>(double, double) -> double;
 
-extern template auto Random::random_angle<float>() -> float;
-extern template auto Random::random_angle<double>() -> double;
+extern template auto Random::angle<float>() -> float;
+extern template auto Random::angle<double>() -> double;
+
+extern template auto Random::point_on_sphere<float>(float) -> glm::vec<3, float>;
+extern template auto Random::point_inside_sphere<float>(float) -> glm::vec<3, float>;
+extern template auto Random::point_on_sphere<double>(double) -> glm::vec<3, double>;
+extern template auto Random::point_inside_sphere<double>(double) -> glm::vec<3, double>;
+
+extern template auto Random::point_on_circle<float>(float) -> glm::vec<2, float>;
+extern template auto Random::point_inside_circle<float>(float) -> glm::vec<2, float>;
+extern template auto Random::point_on_circle<double>(double) -> glm::vec<2, double>;
+extern template auto Random::point_inside_circle<double>(double) -> glm::vec<2, double>;
+
+extern template auto Random::perlin_noise<2, float>(glm::vec<2, float>) -> float;
+extern template auto Random::perlin_noise<3, float>(glm::vec<3, float>) -> float;
+extern template auto Random::perlin_noise<4, float>(glm::vec<4, float>) -> float;
+extern template auto Random::perlin_noise<2, double>(glm::vec<2, double>) -> double;
+extern template auto Random::perlin_noise<3, double>(glm::vec<3, double>) -> double;
+extern template auto Random::perlin_noise<4, double>(glm::vec<4, double>) -> double;
+
+extern template auto Random::normal<float>(float, float) -> float;
+extern template auto Random::normal<double>(double, double) -> double;
+
+extern template auto Random::linear<float>(float, float) -> float;
+extern template auto Random::linear<double>(double, double) -> double;
 
 } // namespace zth
--- zenith/src/core/random.cpp
+++ zenith/src/core/random.cpp
@@ -20,17 +20,40 @@ auto Random::shut_down() -> void
     ZTH_CORE_INFO("Random number generator shut down.");
 }
 
-template auto Random::random_int<u16>(u16, u16) -> u16;
-template auto Random::random_int<i16>(i16, i16) -> i16;
-template auto Random::random_int<u32>(u32, u32) -> u32;
-template auto Random::random_int<i32>(i32, i32) -> i32;
-template auto Random::random_int<u64>(u64, u64) -> u64;
-template auto Random::random_int<i64>(i64, i64) -> i64;
+template auto Random::get_int<u16>(u16, u16) -> u16;
+template auto Random::get_int<i16>(i16, i16) -> i16;
+template auto Random::get_int<u32>(u32, u32) -> u32;
+template auto Random::get_int<i32>(i32, i32) -> i32;
+template auto Random::get_int<u64>(u64, u64) -> u64;
+template auto Random::get_int<i64>(i64, i64) -> i64;
 
-template auto Random::random_float<float>(float, float) -> float;
-template auto Random::random_float<double>(double, double) -> double;
+template auto Random::get_float<float>(float, float) -> float;
+template auto Random::get_float<double>(double, double) -> double;
 
-template auto Random::random_angle<float>() -> float;
-template auto Random::random_angle<double>() -> double;
+template auto Random::angle<float>() -> float;
+template auto Random::angle<double>() -> double;
+
+template auto Random::point_on_sphere<float>(float) -> glm::vec<3, float>;
+template auto Random::point_inside_sphere<float>(float) -> glm::vec<3, float>;
+template auto Random::point_on_sphere<double>(double) -> glm::vec<3, double>;
+template auto Random::point_inside_sphere<double>(double) -> glm::vec<3, double>;
+
+template auto Random::point_on_circle<float>(float) -> glm::vec<2, float>;
+template auto Random::point_inside_circle<float>(float) -> glm::vec<2, float>;
+template auto Random::point_on_circle<double>(double) -> glm::vec<2, double>;
+template auto Random::point_inside_circle<double>(double) -> glm::vec<2, double>;
+
+template auto Random::perlin_noise<2, float>(glm::vec<2, float>) -> float;
+template auto Random::perlin_noise<3, float>(glm::vec<3, float>) -> float;
+template auto Random::perlin_noise<4, float>(glm::vec<4, float>) -> float;
+template auto Random::perlin_noise<2, double>(glm::vec<2, double>) -> double;
+template auto Random::perlin_noise<3, double>(glm::vec<3, double>) -> double;
+template auto Random::perlin_noise<4, double>(glm::vec<4, double>) -> double;
+
+template auto Random::normal<float>(float, float) -> float;
+template auto Random::normal<double>(double, double) -> double;
+
+template auto Random::linear<float>(float, float) -> float;
+template auto Random::linear<double>(double, double) -> double;
 
 } // namespace zth
