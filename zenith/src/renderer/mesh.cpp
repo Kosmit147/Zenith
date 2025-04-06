9be91f3990e86f4c6f29f83c7ef62f6fabcd3c58
Wojciech Opara <wwojtek660@gmail.com>
2025-04-06 23:02:56 +0200

    Add more constructors for Mesh

--- zenith/src/renderer/mesh.cpp
+++ zenith/src/renderer/mesh.cpp
@@ -2,6 +2,25 @@
 
 namespace zth {
 
+Mesh::Mesh(const void* vertex_data, usize vertex_data_size_bytes, usize vertex_stride, const void* index_data,
+           usize index_data_size_bytes, gl::DataType index_data_type, const gl::VertexArrayLayout& layout)
+    : _vertex_buffer(gl::VertexBuffer::create_static_with_data(vertex_data, static_cast<u32>(vertex_data_size_bytes),
+                                                               static_cast<u32>(vertex_stride))),
+      _index_buffer(gl::IndexBuffer::create_static_with_data(index_data, static_cast<u32>(index_data_size_bytes),
+                                                             index_data_type)),
+      _vertex_array(layout, _vertex_buffer, _index_buffer)
+{}
+
+Mesh::Mesh(const void* vertex_data, usize vertex_data_size_bytes, usize vertex_stride, const void* index_data,
+           usize index_data_size_bytes, gl::DataType index_data_type, const gl::VertexArrayLayout& layout,
+           const gl::InstanceBuffer& instance_buffer)
+    : _vertex_buffer(gl::VertexBuffer::create_static_with_data(vertex_data, static_cast<u32>(vertex_data_size_bytes),
+                                                               static_cast<u32>(vertex_stride))),
+      _index_buffer(gl::IndexBuffer::create_static_with_data(index_data, static_cast<u32>(index_data_size_bytes),
+                                                             index_data_type)),
+      _vertex_array(layout, _vertex_buffer, _index_buffer, instance_buffer)
+{}
+
 Mesh::Mesh(Mesh&& other) noexcept
     : _vertex_buffer(std::move(other._vertex_buffer)), _index_buffer(std::move(other._index_buffer)),
       _vertex_array(std::move(other._vertex_array))
