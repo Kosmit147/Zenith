4fd1c0730e05ef5db6a284dfcbc4a008eff74ecd
Wojciech Opara <wwojtek660@gmail.com>
2025-04-07 18:47:57 +0200

    Retrieve OpenGL context variables

--- zenith/include/zenith/gl/context.hpp
+++ zenith/include/zenith/gl/context.hpp
@@ -2,6 +2,8 @@
 
 #include "zenith/core/typedefs.hpp"
 #include "zenith/log/format.hpp"
+#include "zenith/stl/string.hpp"
+#include "zenith/util/result.hpp"
 
 namespace zth {
 
@@ -21,8 +23,41 @@ enum class Profile : u8
     Core,
 };
 
-auto set_debug_context() -> void;
-auto log_context_info() -> void;
+class Context
+{
+public:
+    Context() = delete;
+
+    [[nodiscard]] static auto init() -> Result<void, String>;
+    static auto shut_down() -> void;
+
+    static auto set_debug_context() -> void;
+
+    [[nodiscard]] static auto vendor_string() { return _vendor_string; }
+    [[nodiscard]] static auto renderer_string() { return _renderer_string; }
+    [[nodiscard]] static auto version_string() { return _version_string; }
+    [[nodiscard]] static auto glsl_version_string() { return _glsl_version_string; }
+
+    [[nodiscard]] static auto version() { return _version; }
+
+    [[nodiscard]] static auto forward_compatible_context() { return _forward_compatible_context; }
+    [[nodiscard]] static auto debug_context() { return _debug_context; }
+
+private:
+    static inline const char* _vendor_string = nullptr;
+    static inline const char* _renderer_string = nullptr;
+    static inline const char* _version_string = nullptr;
+    static inline const char* _glsl_version_string = nullptr;
+
+    static inline Version _version;
+
+    static inline bool _forward_compatible_context;
+    static inline bool _debug_context;
+
+private:
+    static auto retrieve_context_variables() -> void;
+    static auto log_context_info() -> void; // log_context_info needs context variables to be retrieved first.
+};
 
 } // namespace gl
 
--- zenith/src/gl/context.cpp
+++ zenith/src/gl/context.cpp
@@ -58,34 +58,76 @@ auto debug_callback([[maybe_unused]] GLenum source, GLenum type, [[maybe_unused]
 
 } // namespace
 
-auto set_debug_context() -> void
+auto Context::init() -> Result<void, String>
+{
+    ZTH_INTERNAL_INFO("Initializing OpenGL context...");
+
+    retrieve_context_variables();
+
+#if !defined(ZTH_DIST_BUILD)
+    set_debug_context();
+#endif
+
+    log_context_info();
+
+    ZTH_INTERNAL_INFO("OpenGL context initialized.");
+    return {};
+}
+
+auto Context::shut_down() -> void
+{
+    ZTH_INTERNAL_INFO("OpenGL context shut down.");
+}
+
+auto Context::set_debug_context() -> void
 {
     glEnable(GL_DEBUG_OUTPUT);
     glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
     glDebugMessageCallback(debug_callback, nullptr);
 
     constexpr std::array<GLuint, 1> disabled_messages_api_other = {
-        131185, // buffer detailed info from NVIDIA
+        131185, // Buffer detailed info from NVIDIA.
     };
 
     glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_OTHER, GL_DONT_CARE,
                           static_cast<GLsizei>(disabled_messages_api_other.size()), disabled_messages_api_other.data(),
                           GL_FALSE);
+
+    _debug_context = true;
 }
 
-auto log_context_info() -> void
+auto Context::retrieve_context_variables() -> void
 {
-    auto vendor = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
-    auto renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
-    auto version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
-    auto glsl_version = reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+    {
+        _vendor_string = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+        _renderer_string = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+        _version_string = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+        _glsl_version_string = reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+    }
 
+    {
+        GLint major, minor;
+        glGetIntegerv(GL_MAJOR_VERSION, &major);
+        glGetIntegerv(GL_MINOR_VERSION, &minor);
+        _version = Version{ .major = static_cast<u32>(major), .minor = static_cast<u32>(minor) };
+    }
+
+    {
+        GLint flags;
+        glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+        _forward_compatible_context = flags & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT;
+        _debug_context = flags & GL_CONTEXT_FLAG_DEBUG_BIT;
+    }
+}
+
+auto Context::log_context_info() -> void
+{
     ZTH_INTERNAL_INFO("[OpenGL] Context info:\n"
                       "\tVendor: {}\n"
                       "\tRenderer: {}\n"
                       "\tVersion: {}\n"
                       "\tGLSL Version: {}",
-                      vendor, renderer, version, glsl_version);
+                      _vendor_string, _renderer_string, _version_string, _glsl_version_string);
 }
 
 } // namespace gl
