34837dcef7f5fa33b67f3b39ef39fe3a251c1227
Wojciech Opara <wwojtek660@gmail.com>
2025-04-28 13:54:00 +0200

    Update embed submodule

--- testbed/src/embedded.cpp
+++ testbed/src/embedded.cpp
@@ -4,12 +4,12 @@
 
 namespace embedded {
 
-const std::span<const std::uint8_t> cobble_diffuse_map_data = b::embed<"assets/cobble_diffuse.png">().data();
-const std::span<const std::uint8_t> container_diffuse_map_data = b::embed<"assets/container_diffuse.jpg">().data();
-const std::span<const std::uint8_t> container2_diffuse_map_data = b::embed<"assets/container2_diffuse.png">().data();
-const std::span<const std::uint8_t> container2_specular_map_data = b::embed<"assets/container2_specular.png">().data();
-const std::span<const std::uint8_t> emoji_diffuse_map_data = b::embed<"assets/emoji_diffuse.png">().data();
-const std::span<const std::uint8_t> matrix_emission_map_data = b::embed<"assets/matrix_emission.jpg">().data();
-const std::span<const std::uint8_t> wall_diffuse_map_data = b::embed<"assets/wall_diffuse.jpg">().data();
+const std::span<const std::byte> cobble_diffuse_map_data = b::embed<"assets/cobble_diffuse.png">().data();
+const std::span<const std::byte> container_diffuse_map_data = b::embed<"assets/container_diffuse.jpg">().data();
+const std::span<const std::byte> container2_diffuse_map_data = b::embed<"assets/container2_diffuse.png">().data();
+const std::span<const std::byte> container2_specular_map_data = b::embed<"assets/container2_specular.png">().data();
+const std::span<const std::byte> emoji_diffuse_map_data = b::embed<"assets/emoji_diffuse.png">().data();
+const std::span<const std::byte> matrix_emission_map_data = b::embed<"assets/matrix_emission.jpg">().data();
+const std::span<const std::byte> wall_diffuse_map_data = b::embed<"assets/wall_diffuse.jpg">().data();
 
 } // namespace embedded
--- testbed/src/embedded.hpp
+++ testbed/src/embedded.hpp
@@ -2,12 +2,12 @@
 
 namespace embedded {
 
-extern const std::span<const std::uint8_t> cobble_diffuse_map_data;
-extern const std::span<const std::uint8_t> container_diffuse_map_data;
-extern const std::span<const std::uint8_t> container2_diffuse_map_data;
-extern const std::span<const std::uint8_t> container2_specular_map_data;
-extern const std::span<const std::uint8_t> emoji_diffuse_map_data;
-extern const std::span<const std::uint8_t> matrix_emission_map_data;
-extern const std::span<const std::uint8_t> wall_diffuse_map_data;
+extern const std::span<const std::byte> cobble_diffuse_map_data;
+extern const std::span<const std::byte> container_diffuse_map_data;
+extern const std::span<const std::byte> container2_diffuse_map_data;
+extern const std::span<const std::byte> container2_specular_map_data;
+extern const std::span<const std::byte> emoji_diffuse_map_data;
+extern const std::span<const std::byte> matrix_emission_map_data;
+extern const std::span<const std::byte> wall_diffuse_map_data;
 
 } // namespace embedded
