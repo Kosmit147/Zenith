a457ac835391c90bbdea03cd4a21243fa718c449
Wojciech Opara <wwojtek660@gmail.com>
2025-05-17 23:12:13 +0200

    Remove exception and stdexcept from pch

--- zenith/src/core/exception.cpp
+++ zenith/src/core/exception.cpp
@@ -5,11 +5,11 @@
 namespace zth {
 
 ZTH_NO_INLINE Exception::Exception(const char* message, usize skip)
-    : std::runtime_error(message), _stacktrace(std::stacktrace::current(skip + 1))
+    : std::runtime_error{ message }, _stacktrace{ std::stacktrace::current(skip + 1) }
 {}
 
 ZTH_NO_INLINE Exception::Exception(const String& message, usize skip)
-    : std::runtime_error(message), _stacktrace(std::stacktrace::current(skip + 1))
+    : std::runtime_error{ message }, _stacktrace{ std::stacktrace::current(skip + 1) }
 {}
 
 auto Exception::stacktrace() const -> String
