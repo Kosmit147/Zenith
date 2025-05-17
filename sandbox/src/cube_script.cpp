3dee49a4ba62dc1a927ae3a943b6e35119d94043
Wojciech Opara <wwojtek660@gmail.com>
2025-05-17 17:18:18 +0200

    Fix build errors

--- sandbox/src/cube_script.cpp
+++ sandbox/src/cube_script.cpp
@@ -1,5 +1,7 @@
 #include "cube_script.hpp"
 
+namespace scripts {
+
 auto Cube::on_update(zth::EntityHandle actor) -> void
 {
     const auto time = zth::Time::time<float>();
@@ -9,3 +11,5 @@ auto Cube::on_update(zth::EntityHandle actor) -> void
     transform.rotate(0.0005f * time, glm::normalize(glm::vec3{ 0.0f, 1.0f, 0.0f }));
     transform.rotate(0.0005f * time, glm::normalize(glm::vec3{ -0.3f, 0.1f, 0.7f }));
 }
+
+} // namespace scripts
--- sandbox/src/main_scene.cpp
+++ sandbox/src/main_scene.cpp
@@ -8,14 +8,6 @@ namespace {
 
 const auto wall_texture = b::embed<"assets/wall.jpg">().data();
 
-const auto camera_transform_component =
-    zth::TransformComponent{ glm::vec3{ 0.0f, 0.0f, 5.0f }, zth::math::world_forward };
-
-const auto camera_camera_component = zth::CameraComponent{
-    .aspect_ratio = 16.0f / 9.0f,
-    .fov = glm::radians(45.0f),
-};
-
 } // namespace
 
 MainScene::MainScene()
@@ -23,16 +15,15 @@ MainScene::MainScene()
       _cube_texture{ std::make_shared<zth::gl::Texture2D>(zth::gl::Texture2D::from_file_data(wall_texture)) },
       _cube_material{ std::make_shared<zth::Material>(zth::Material{ .diffuse_map = _cube_texture }) }
 {
-    _camera.emplace_or_replace<zth::TransformComponent>(camera_transform_component);
-    _camera.emplace_or_replace<zth::CameraComponent>(camera_camera_component);
+    _camera.transform().translate(glm::vec3{ 0.0f, 0.0f, 5.0f });
+    _camera.emplace_or_replace<zth::CameraComponent>();
     _camera.emplace_or_replace<zth::ScriptComponent>(zth::make_unique<zth::scripts::FlyCamera>());
 
     _cube.emplace_or_replace<zth::MeshRendererComponent>(zth::meshes::cube());
     _cube.emplace_or_replace<zth::MaterialComponent>(_cube_material);
 
-    _light.emplace_or_replace<zth::TransformComponent>(glm::vec3{ 0.0f },
-                                                       glm::normalize(glm::vec3{ 0.0f, -1.0f, -1.0f }));
+    _light.transform().set_direction(glm::normalize(glm::vec3{ 0.0f, -1.0f, -1.0f }));
     _light.emplace_or_replace<zth::LightComponent>(zth::DirectionalLight{});
 
-    _cube.emplace_or_replace<zth::ScriptComponent>(zth::make_unique<CubeScript>());
+    _cube.emplace_or_replace<zth::ScriptComponent>(zth::make_unique<scripts::Cube>());
 }
